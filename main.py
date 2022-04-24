041f1f66ac7b1388da1bfa963480d4d2daae8b85
zykon2004 <zykon@pm.me>
2022-04-24 22:54:10 +0300

    generated paired results with more info

--- main.py
+++ main.py
@@ -13,18 +13,17 @@ def main():
   players_with_behavior = 3
   q_num = 100
   behavior = BehavioralAttribute(name='COVID19', shape='hexagon')
-  pairs=False
+  pairs=True
   number_of_samples=100
-  
   simulators = Simulator.generate_simulators(
     players=players, 
     players_with_behavior=players_with_behavior,
     q_num=q_num, 
-    behavior=behavior,
+    behavior=behavior, 
     )
   
   df = play_simulator(tuple(simulators), pairs=pairs, number_of_samples=number_of_samples)
-  df.to_excel(f'generated/raw/results_{players}_{players_with_behavior}_{q_num}{"_pairs" if pairs else ""}.xlsx')
+  df.to_excel(f'generated/raw/results_{players}_{p}_{q_num}{"_pairs" if pairs else ""}.xlsx')
 
 
 def play_simulator(simulators, 
